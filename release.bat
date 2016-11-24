8210b4db0fc45bf210a2e272fe806ba78dc618b1
darkconsole <darkcee.legit@gmail.com>
2016-11-24 01:54:07 -0600

    a friendly reminder for myself

--- release.bat
+++ release.bat
@@ -1,1 +1,3 @@
-zip dcc-soulgem-oven-v%~1.zip -r interface meshes scripts seq textures dcc-soulgem-oven-000.esm
+@echo off
+echo DID YOU INCREMEMENT THE VERSION NUMBER IN THE MAIN SCRIPT ASSHOLE?
+zip dcc-soulgem-oven-v%~1.zip -q -r interface meshes scripts seq textures dcc-soulgem-oven-000.esm
